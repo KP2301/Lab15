359a10fe5279a340d265d36eb4025c90b43f5110
Kittiwin <kittiwinktb@gmail.com>
2023-01-17 15:58:55 +0700

    lab15_2 complete

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,33 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[][N]){
+	for(int i = 0; i < N ; i++){
+		cout << "Row " << i+1 << ": ";
+        for(int j = 0; j < N; j++){
+            cin >> A[i][j];
+        }
+    }
+}
+
+void showMatrix(const bool A[][N]){
+	for(int i = 0; i < N ; i++){
+        for(int j = 0; j < N; j++){
+            cout << A[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if(a[i][j] >= a[i+1][j] && a[i][j] >= a[i-1][j] && a[i][j] >= a[i][j+1] && a[i][j] >= a[i][j-1]) b[i][j] = true;
+			else b[i][j] = false;
+			if(i == 0 || j == N - 1 || i == N - 1 || j == 0){
+				b[i][j] = false; 
+			}
+		}
+	}
+}
